e7b17a8685eca0e6651b870861d3e7b9eafbe959
Harsh <harsh.619.cr7@.com>
2023-09-04 13:43:01 +0530

    Icons Added

--- src/app/page.js
+++ src/app/page.js
@@ -3,7 +3,7 @@ import ImageCard from '../components/atoms/ImageCard';
 export default function Home() {
   return (
     <div>
-    {/* <ImageCard icon="flex"/> */}
+    <ImageCard icon="flex"/>
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
     Hello Hello 
     </main>
