cde5f6eddabe7c807f4e7d9159e0aa20bdd2f6b4
niteshn1 <117146760+niteshn1@users.noreply.github.com>
2023-09-07 20:42:52 +0530

    popularsearch

--- src/components/popular.jsx
+++ src/components/popular.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 import product_card from "@/data/data";
+import { searchLabels } from "@/utils/labels/label";
 
 const Popular = () => {
   // console.log(product_card);
-  const handleAddToWishList = () => {};
   const listItems = product_card.map((item) => (
     <div className="mx-7 w-500 mb-12" key={item.id}>
       <div className="w-72 relative">
         <img src={item.thumb} alt="suit" />
-        <div className="absolute top-3 right-3" onClick={handleAddToWishList}>
+        <div className="absolute top-3 right-3">
           <img src={item.wish} alt="suit" />
         </div>
       </div>
@@ -33,7 +33,7 @@ const Popular = () => {
   return (
     <div className="py-5 max-w-full mx-auto max-w-screen-full text-center lg:py-7">
       <h2 className="text-3xl font-medium mb-2 ml-10 text-left">
-        popular Searches
+        {searchLabels.heading}
       </h2>
       <div className=" w-calc(100% / 3) max-w-screen-full lg flex flex-nowrap mx-5 my-5 overflow-x-auto  ">
         {listItems}
---
+++ src/utils/labels/label.js
@@ -0,0 +1,3 @@
+export const searchLabels = {
+  heading: "popular searches",
+};
