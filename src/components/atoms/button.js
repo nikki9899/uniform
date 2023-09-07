4caf8a2bac75fc7826de7bb2f873948ada67b8eb
Sourabh <sourabh6712@gmail.com>
2023-09-07 17:47:45 +0530

    Added buttons

--- src/components/atoms/button.js
+++ src/components/atoms/button.js
@@ -1,37 +1,23 @@
 import React from "react";
 
-function PrimaryCircularButton({ text }) {
+function Button({ variant, rounded, onClick }) {
+  let className =
+    "text-gray-900 font-manrope  lowercase bg-transparent border";
+
+  if (rounded === "Full") {
+    className += " rounded-full text-xs font-medium py-2 px-4  border-gray-700";
+  } else if (rounded === "semi") {
+    className += " rounded-lg text-24 font-normal px-4 py-2  border-gray-700";
+  } else {
+    className +=
+      " text-white text-24 font-normal lowercase bg-[#C46145] border-none p-4 space-x-2 rounded-lg font-manrope";
+  }
+
   return (
-    <div>
-      <button className="inline-flex h-8.108 py-2 px-4 justify-center items-center gap-8.103 flex-shrink-0 rounded-full border border-gray-700">
-        <p className="text-gray-900 font-manrope text-xs font-medium lowercase">
-          {text}
-        </p>
-      </button>
-    </div>
+    <button className={className} onClick={onClick}>
+      {variant}
+    </button>
   );
 }
 
-function PrimarySquareButton({ text }) {
-  return (
-    <div>
-      <button className="bg-transparent inline-flex px-4 py-2 items-center gap-2 rounded-lg border border-gray-700 ">
-        <p className="text-gray-900 font-manrope text-24 font-normal lowercase">
-          {text}
-        </p>
-      </button>
-    </div>
-  );
-}
-
-function SecondaryButton({ text }) {
-  return (
-    <div>
-      <button className="bg-c46145 inline-flex p-4 space-x-2 items-center rounded-lg font-manrope">
-        <p className="text-white  text-24 font-normal lowercase">{text}</p>
-      </button>
-    </div>
-  );
-}
-
-export { PrimaryCircularButton, PrimarySquareButton, SecondaryButton };
+export default Button;
