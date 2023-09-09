f232013edc7be8db4a2392b49f03ca10bd21d43f
Sourav Joshi <hudasourav@gmail.com>
2023-09-09 10:32:14 +0530

    quick fix

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,7 +3,7 @@ import './globals.css'
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en" className={'bg-background'}>  
+    <html lang="en">  
       <body>{children}
       <Footer/>
       </body>
