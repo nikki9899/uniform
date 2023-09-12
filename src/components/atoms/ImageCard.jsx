f1276bea8ff8624318280994078c0acbe5844fc7
Harsh <harsh.619.cr7@.com>
2023-09-12 15:10:24 +0530

    Image Card Updated

--- src/components/atoms/ImageCard.jsx
+++ src/components/atoms/ImageCard.jsx
@@ -1,15 +1,7 @@
-import Search from "./Icons/Search";
 import Wish from "./Icons/Wish";
 const ImageCard = ({ iconDisplay, classnames }) => {
   return (
     <div className={classnames}>
-      <Search
-        width="24"
-        height="24"
-        fill="#1E1E1E"
-        viewBox="0 0 24 24"
-        classnames="float-right mr-2 mt-2 cursor-pointer"
-      />
       <Wish
         width="24"
         height="24"
