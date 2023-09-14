365f5fbf44e63078439537cc1d3237b06545f520
Sourav Joshi <hudasourav@gmail.com>
2023-09-14 09:40:50 +0530

    quick fix

--- src/components/atoms/Pagination/index.jsx
+++ src/components/atoms/Pagination/index.jsx
@@ -10,18 +10,30 @@ const Pagination = ({
 }) => {
     return (
         <div className="flex items-center gap-x-4">
-            <CircularButton backgroundColor="white" border={true} onClick = {prevPageHandler}>
+            <CircularButton
+                backgroundColor="white"
+                border={true}
+                onClick={prevPageHandler}
+            >
                 <Carrot className="-rotate-90 text-xl" />
-                <span className='absolute bottom-0 left-0 right-0 translate-y-[102%] mx-auto text-xs font-medium'>back</span>
+                <span className="absolute bottom-0 left-0 right-0 translate-y-[100%] pt-1 mx-auto text-xs font-medium">
+                    back
+                </span>
             </CircularButton>
 
-            <div className='text-base font-medium font-manrope'>
+            <div className="text-base font-medium font-manrope">
                 {currPage} of {totalPage}
             </div>
 
-            <CircularButton backgroundColor="white" border={true} onClick = {nextPageHandler}>
+            <CircularButton
+                backgroundColor="white"
+                border={true}
+                onClick={nextPageHandler}
+            >
                 <Carrot className="rotate-90 text-xl" />
-                <span className='absolute bottom-0 left-0 right-0 translate-y-[102%] mx-auto text-xs font-medium'>next</span>
+                <span className="absolute bottom-0 left-0 right-0 translate-y-[100%] pt-1 mx-auto text-xs font-medium">
+                    next
+                </span>
             </CircularButton>
         </div>
     )
