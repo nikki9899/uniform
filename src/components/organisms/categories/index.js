a250240de8052917eeeb8f8e0bb87e6ff0b749b9
homosapien14 <spma259@gmail.com>
2023-09-14 16:51:40 +0530

    made all required changes

--- src/components/organisms/categories/index.js
+++ src/components/organisms/categories/index.js
@@ -1,22 +1,27 @@
+import Link from 'next/link'
 import ImageCard from '@/components/atoms/ImageCard'
 import Button from '@/components/atoms/button'
+import Grid from '@/components/molecules/grid'
 import { categories } from '@/mockData/categories'
 
 const Categories = () => {
     return (
-        <div className="w-full h-full grid grid-cols-4 gap-4">
-            {categories.map(({ title, image }) => (
-                <div className="">
+        <Grid cardsPerRow={4}>
+            {categories.map(({ title, image, id }) => (
+                <div key={id}>
                     <ImageCard
                         iconDisplay="hidden"
                         classnames={`w-64 h-80 rounded-2xl shrink-0 back  bg-[url('/Images/productImage1.png')]`}
                     />
                     <h5 className="mt-2 mb-2">{title}</h5>
-                    {/* need to discuss about explore button Link */}
-                    <Button rounded="Full" variant="explore" />
+                    <Link href="#">
+                        <Button rounded="full" variant="primary">
+                            explore
+                        </Button>
+                    </Link>
                 </div>
             ))}
-        </div>
+        </Grid>
     )
 }
 
--- src/mockData/categories.js
+++ src/mockData/categories.js
@@ -1,55 +1,66 @@
 export const categories =[
     {
+        id:1,
         title:"Full body safety suit",
         image:"/Images/productImage1.png",
         explore:""
     },
     {
+        id:2,
         title:"Rubber Suit",
         image:"/Images/productImage1.png",
         explore:""
     },
     {
+        id:3,
         title:"Industrial Aprons",
         image:"/Images/productImage1.png",
         explore:""
     },
     {
+        id:4,
         title:"Chemical Resistance Clothing",
         image:"/Images/productImage1.png",
         explore:""
     },
     {
+        id:5,
         title:"Full body safety suit",
         image:"/Images/productImage1.png",
         explore:""
     },
     {
+        id:6,
         title:"Full body safety suit",
         image:"/Images/productImage1.png",
         explore:""
     },
     {
+        id:7,
         title:"Full body safety suit",
         image:"/Images/productImage1.png",
         explore:""
     },
     {
+        id:8,
         title:"Full body safety suit",
         image:"/Images/productImage1.png",
         explore:""
     },
     {
+        id:9,
         title:"Full body safety suit",
         image:"/Images/productImage1.png",
         explore:""
     },
     {
+        id:10,
         title:"Full body safety suit",
         image:"/Images/productImage1.png",
         explore:""
     },
     {
+        id:11,
         title:"Full body safety suit",
         image:"/Images/productImage1.png",
         explore:""
