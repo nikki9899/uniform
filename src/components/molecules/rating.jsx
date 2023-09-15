ca893afb1d0e639c9e0d99acecf8347220554547
Dipesh  Pawar <dipesh.p@renewhr.com>
2023-09-15 11:36:46 +0530

    fix

--- src/components/molecules/rating.jsx
+++ src/components/molecules/rating.jsx
@@ -3,19 +3,13 @@ import { ChatIcon } from '@svgs/index'
 import { Star } from '@svgs/index'
 
 const Rating = (props) => {
-    let { rating = 0, review = 0 } = props?.productRating
-    let ratingStar = []
-
-    for (let i = 0; i < 5; i++) {
-        let color = i < rating ? '#E9DB15' : '#4b5563'
-        ratingStar.push({ color })
-    }
-
+    let { rating = 5, review = 0 } = props?.productRating
+    let ratingStar = [ 1,2,3,4,5 ]
     return (
         <div className="flex content-center items-center">
             <div className="flex flex-row w-35 mx-2">
-                {ratingStar.map(({ color }, index) => {
-                    return <Star key={index} fill={color} />
+                {ratingStar.map(( item, index) => {
+                    return <Star key={item} fill={( index < rating ? '#E9DB15' : '#4b5563') } />
                 })}
             </div>
             <div className="flex-initial text-sm font-semibold">{rating}</div>
