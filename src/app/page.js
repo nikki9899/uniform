a403036ced23be3f46f9c8ec9cbb293a63e7e830
Harsh <harsh.619.cr7@.com>
2023-09-15 14:57:04 +0530

    Section updated

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,4 @@
-import ImageCard from '../components/atoms/ImageCard';
-import PopularCard from '../components/atoms/PopularCard'
-import PopularProducts from '@/components/molecules/popularProducts';
+import PopularProducts from '@/components/organisms/popularProducts';
 
 export default function Home() {
 
@@ -8,11 +6,6 @@ export default function Home() {
     <div>
       <main className="flex min-h-screen flex-col items-center justify-between p-[60px]">
         <PopularProducts />
-        {/* <PopularCard productTitle="Black Full body suit" productPrice="5000 INR" productType="full body resistance"/>
-        {/* <ImageCard */}
-          {/* iconDisplay="flex" 
-          classnames="w-[308px] h-[418px] rounded-2xl shrink-0 back ml-2 bg-[url('/Images/productImage1.png')]"
-        /> */} 
       </main>
     </div>
   );
---
+++ src/components/molecules/PopularCard/index.jsx
@@ -0,0 +1,34 @@
+import ImageCard from '@/components/atoms/ImageCard'
+import Button from '@/components/atoms/button'
+
+const PopularCard = ({
+    productTitle,
+    productPrice,
+    productType,
+    productImage,
+}) => {
+    return (
+        <div>
+            <ImageCard
+                iconDisplay="flex"
+                classnames="w-[280px] h-[380px] rounded-2xl shrink-0 back ml-2"
+                productImage={productImage}
+            />
+            <div className="mt-2">
+                <span className="pl-2 text-popularTextColor text-center text-sm not-italic font-bold leading-[26.645px]">
+                    {productTitle}
+                </span>
+                <span className="text-popularTextColor text-center text-sm not-italic font-bold leading-[26.645px] float-right">
+                    {productPrice}
+                </span>
+            </div>
+            <div className="text-popularType w-full mb-2 text-xs not-italic font-medium leading-[26.645px] float-left pl-2 mt-0 pt-0">
+                {productType}
+            </div>
+            <div className="pl-2">
+                <Button variant="enquire now" rounded="Full" />
+            </div>
+        </div>
+    )
+}
+export default PopularCard
