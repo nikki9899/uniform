05cd76cd9d6df6d0803f439aa9c5e5e0e1f6a8d8
Harsh <harsh.619.cr7@.com>
2023-09-19 14:51:09 +0530

    Navbar Created

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,10 +1,13 @@
 import Footer from '@/components/footer'
+import Navbar from '@/components/molecules/navbar';
 import './globals.css'
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">  
-      <body>{children}
+    <html lang="en"> 
+      <body>
+      <Navbar/> 
+      {children}
       <Footer/>
       </body>
     </html>
---
+++ src/utils/labels/navbarLabels.js
@@ -0,0 +1,26 @@
+export const NavbarLabels = {
+    Logo : "UniformOnWeb",
+    Tabs : [
+        {
+            id : "001",
+            TabTitle : "Uniform by Profession",
+            src : "https://www.google.com/"
+        },
+        {
+            id : "002",
+            TabTitle : "Sale",
+            src : "https://www.google.com/"
+        },
+        {
+            id : "003",
+            TabTitle : "About",
+            src : "https://www.google.com/"
+        },
+        {
+            id : "004",
+            TabTitle : "Contact Us",
+            src : "https://www.google.com/"
+        }],
+    SearchLink : "https://www.google.com/",
+    WishLink : "https://www.youtube.com/"
+};
