a5c38dc6c3d3d8d9b3ffbe03047db5f19323439b
homosapien14 <spma259@gmail.com>
2023-09-19 16:36:18 +0530

    category page created

--- src/components/organisms/categories/index.js
+++ src/components/organisms/categories/index.js
@@ -3,27 +3,30 @@ import ImageCard from '@/components/atoms/ImageCard'
 import Button from '@/components/atoms/button'
 import Grid from '@/components/molecules/grid'
 import { categories } from '@/mockData/categories'
-import categoriesLabels from '@/utils/labels/categoriesLabels';
+import { categoriesLabels } from '@/utils/labels/categoriesLabels'
 
 const Categories = () => {
-    const {linkText} = categoriesLabels;
+    const { linkText } = categoriesLabels
     return (
-        <Grid cardsPerRow={4}>
-            {categories.map(({ title, image, id }) => (
-                <div key={id}>
-                    <ImageCard
-                        iconDisplay="hidden"
-                        classnames={`w-64 h-80 rounded-2xl shrink-0 back  bg-[url('/Images/productImage1.png')]`}
-                    />
-                    <h5 className="mt-2 mb-2">{title}</h5>
-                    <Link href="#">
-                        <Button rounded="full" variant="primary">
-                            {linkText}
-                        </Button>
-                    </Link>
-                </div>
-            ))}
-        </Grid>
+        <div className="container my-20">
+            <Grid cardsPerRow={4}>
+                {categories.map(({ title, image, id }) => (
+                    <div key={id}>
+                        <ImageCard
+                            iconDisplay="hidden"
+                            classnames={`w-64 h-80 rounded-2xl shrink-0 back `}
+                            productImage={image}
+                        />
+                        <h5 className="mt-2 mb-2">{title}</h5>
+                        <Link href="#">
+                            <Button rounded="full" variant="primary">
+                                {linkText}
+                            </Button>
+                        </Link>
+                    </div>
+                ))}
+            </Grid>
+        </div>
     )
 }
 
