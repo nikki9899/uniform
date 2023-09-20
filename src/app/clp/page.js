79fb116f3d4e19466568d353ff06d8bde8092aa2
Dipesh  Pawar <dipesh.p@renewhr.com>
2023-09-20 17:13:06 +0530

    fix

---
+++ src/app/clp/page.js
@@ -0,0 +1,23 @@
+import Categories from '@/components/organisms/categories'
+import Pagination from '@/components/atoms/Pagination'
+import Popular from '@/components/popular'
+
+export const metadata = {
+    title: 'Categories',
+}
+
+const Clp = () => {
+    return (
+        <div>
+            <main className="flex min-h-screen flex-col items-center justify-between p-16">
+                <div className="h-px w-full  bg-black border-0 "></div>
+                <Categories />
+                <Pagination totalPage={10} currPage={1} />
+                <div className="h-px w-full my-8 bg-black border-0 "></div>
+                <Popular />
+            </main>
+        </div>
+    )
+}
+
+export default Clp
