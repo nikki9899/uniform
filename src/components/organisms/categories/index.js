c365853f83f17ab96dec2baba35e96942454f08f
homosapien14 <spma259@gmail.com>
2023-09-20 11:40:40 +0530

    integrated backend API for categories section

--- src/components/organisms/categories/index.js
+++ src/components/organisms/categories/index.js
@@ -2,22 +2,35 @@ import Link from 'next/link'
 import ImageCard from '@/components/atoms/ImageCard'
 import Button from '@/components/atoms/button'
 import Grid from '@/components/molecules/grid'
-import { categories } from '@/mockData/categories'
 import { categoriesLabels } from '@/utils/labels/categoriesLabels'
 
-const Categories = () => {
-    const { linkText } = categoriesLabels
+// all Categories API: https://uniformonweb.onrender.com/api/categories?populate=image
+
+const Categories = async () => {
+    const { linkText } = categoriesLabels;
+    const response = await fetch(
+        'https://uniformonweb.onrender.com/api/categories?populate=image'
+    );
+    const { data } = await response.json()
+    const categories = data.map((entry) => {
+        return {
+            id: entry.id,
+            name: entry.attributes.name,
+            image: entry.attributes.image.data.attributes.url,
+        }
+    })
+
     return (
         <div className="container my-20">
             <Grid cardsPerRow={4}>
-                {categories.map(({ title, image, id }) => (
+                {categories.map(({ name, image, id }) => (
                     <div key={id}>
                         <ImageCard
                             iconDisplay="hidden"
                             classnames={`w-64 h-80 rounded-2xl shrink-0 back `}
                             productImage={image}
                         />
-                        <h5 className="mt-2 mb-2">{title}</h5>
+                        <h5 className="mt-2 mb-2">{name}</h5>
                         <Link href="#">
                             <Button rounded="full" variant="primary">
                                 {linkText}
