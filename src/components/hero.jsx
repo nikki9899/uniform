e620d07cf348e13921a424b38b43b47f59b6a2fc
nikki9899 <nikkidagar5316@gmail.com>
2023-09-21 16:03:20 +0530

    updated hero

--- src/components/hero.jsx
+++ src/components/hero.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { heroLabels } from "@/utils/labels/heroLabels";
-
 import Button from "./atoms/button";
 
 function Hero() {
@@ -10,16 +9,19 @@ function Hero() {
     <div className=" relative">
       <div className=" text-center font-manrope text-8xl font-medium leading-[152px] tracking-[-4.6px]">
         <h2>{heading}</h2>
-        <div className=" -z-10 absolute top-44 ml-[165px] left-0 transform -translate-x-1/2 -translate-y-1/2">
+        <div className=" -z-10 absolute top-44 ml-[390px] left-0 transform -translate-x-1/2 -translate-y-1/2">
           <img src="/Images/Doctor.png" />
         </div>
         <h3 className="pl-[301px]">{headingPart2}</h3>
       </div>
-      <div className="ml-36 mt-8  text-gray-900 text-4xl  font-poppins leading-[26.645px] lowercase">
+      <div className="ml-96 mt-7  text-gray-900 text-4xl  font-poppins leading-[26.645px] lowercase">
         <p>{description}</p>
       </div>
-      <div className=" h-[91px] flex justify-center items-center">
-        <Button variant="enquire now" rounded="semi" />
+      <div className="mt-[73px] ml-[540px] h-[91px]  ">
+        <Button variant="primary" rounded="semi" >
+          <p>enquire now</p>
+        </Button>
+          
       </div>
     </div>
   );
--- src/components/industrialUniform.jsx
+++ src/components/industrialUniform.jsx
@@ -8,7 +8,7 @@ const IndustrialUniform = () => {
       <div className=" mb-[44px] text-center text-black text-[115px] not-italic font-medium leading-[80.724px] tracking-[-4.6px] font-manrope">
         <h1>{heading}</h1>
       </div>
-      <div className="  text-center text-gray-700 text-[34px] not-italic font-normal leading-[26.645px] lowercase font- Poppins;">
+      <div className="  text-center  text-gray-700 text-[34px] not-italic font-normal leading-[26.645px] lowercase font- Poppins;">
         <p>{description}</p>
       </div>
     </div>
