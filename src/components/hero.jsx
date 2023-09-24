82a1c7519a1680ada6c2b8c76d26a35055d0b403
Dipesh  Pawar <dipesh.p@renewhr.com>
2023-09-24 19:29:07 +0530

    fix hero

--- src/components/hero.jsx
+++ src/components/hero.jsx
@@ -6,10 +6,10 @@ function Hero() {
   let { heading, headingPart2, description } = heroLabels;
 
   return (
-    <div className=" relative">
-      <div className=" text-center font-manrope text-8xl font-medium leading-[152px] tracking-[-4.6px]">
+    <div className="relative py-4 mt-20">
+      <div className=" text-center font-manrope text-[115px] font-normal leading-[152px] tracking-[-4.6px]">
         <h2>{heading}</h2>
-        <div className=" -z-10 absolute top-44 left-96 transform -translate-x-1/2 -translate-y-1/2">
+        <div className=" -z-10 absolute top-48 left-96 transform -translate-x-1/2 -translate-y-1/2">
           <img src="/Images/Doctor.png" />
         </div>
         <h3 className="pl-[301px]">{headingPart2}</h3>
@@ -19,7 +19,7 @@ function Hero() {
       </div>
       <div className="text-center my-10 ">
         <Button variant="primary" rounded="semi" >
-          <p>enquire now</p>
+          <p className="p-1 text-2xl font-light" >enquire now</p>
         </Button>
           
       </div>
