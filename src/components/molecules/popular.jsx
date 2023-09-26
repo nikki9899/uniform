74fbfe3796f5996876bd018d42d1d27f7548a999
niteshn1 <117146760+niteshn1@users.noreply.github.com>
2023-09-26 09:49:46 +0530

    dynamicdata

--- src/components/molecules/popular.jsx
+++ src/components/molecules/popular.jsx
@@ -1,39 +1,44 @@
 import Button from '@/components/atoms/button'
 import React from 'react'
-import product_card from '@/data/data'
 import ImageCard from '@/components/atoms/ImageCard'
 import { searchLabels } from '@/utils/labels/label'
+import { getAPI } from '@/utils/api'
 
-const Popular = (props) => {
-    const items = product_card.map(
-        ({ id, product_name, description, product_image, product_price }) => (
-            <div className=" me-7 w-full mb-12" key={id}>
-                <ImageCard
-                    iconDisplay="flex"
-                    classnames="w-[19rem] h-[26rem] rounded-2xl shrink-0 back ml-2"
-                    productImage={product_image}
-                />
-                <div>
-                    <div className="flex justify-between my-1 ml-2">
-                        <h3 className="text-sm font-semibold leading-7">
-                            {product_name}
-                        </h3>
-                        <h4 className="text-sm font-semibold leading-7">
-                            {product_price}
-                        </h4>
-                    </div>
-                    <p className="text-xs font-medium ml-2 text-start text-gray-500 leading-7">
-                        {description}
-                    </p>
-                    <div className="text-left ml-2 my-2">
-                        <Button variant="primary" rounded="full">
-                            {searchLabels.buttontext}
-                        </Button>
-                    </div>
+const Popular = async (props) => {
+    const { data } = await getAPI('products')
+    const products = data.map((entry) => {
+        return {
+            id: entry.id,
+            title: entry.attributes.Title,
+            image: entry.attributes.image.data.attributes.url,
+            description: entry.attributes.description,
+        }
+    })
+    const items = products.map(({ title, image, id, description }) => (
+        <div className=" me-7 w-full mb-12" key={id}>
+            <ImageCard
+                iconDisplay="flex"
+                classnames="w-[19rem] h-[26rem] rounded-2xl shrink-0 back ml-2"
+                productImage={image}
+            />
+            <div>
+                <div className="flex justify-between my-1 ml-2">
+                    <h3 className="text-sm font-semibold leading-7">{title}</h3>
+                    <h4 className="text-sm font-semibold leading-7">
+                        INR 5000
+                    </h4>
+                </div>
+                <p className="text-xs font-medium ml-2 text-start text-gray-500 leading-7">
+                    {description}
+                </p>
+                <div className="text-left ml-2 my-2">
+                    <Button variant="primary" rounded="full">
+                        {searchLabels.buttontext}
+                    </Button>
                 </div>
             </div>
-        )
-    )
+        </div>
+    ))
     return (
         <div className="py-5 max-w-full  max-w-screen-full text-center lg:py-7">
             <div className="text-3xl font-medium mb-2 text-left">
