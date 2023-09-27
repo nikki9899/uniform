92adc77248975675aaf810ee2ddce010d1e1b8a2
Dipesh  Pawar <dipesh.p@renewhr.com>
2023-09-27 19:52:29 +0530

    with mock data

--- src/app/clp/[category]/page.js
+++ src/app/clp/[category]/page.js
@@ -3,13 +3,16 @@ import Pagination from '@/components/atoms/Pagination'
 import Popular from '@/components/molecules/popular'
 import IndustrialUniform from '@/components/industrialUniform'
 import { getAPI, getSubCategories } from '@/utils/api'
+import { industrialCategoryMockData } from '@/mockData/category'
 export const metadata = {
     title: 'Categories',
 }
 
 const Clp =  async ( { params : {category}  } ) => {
-   const { data = [] , meta : { pagination : {  page  , pageSize ,pageCount , total}} } =  await getSubCategories(category) ;
-
+   let  { data = [] , meta : { pagination : {  page  , pageSize ,pageCount , total}} } =  await getSubCategories(category) ;
+if(data.length == 0 ) {
+    data = industrialCategoryMockData 
+}
     return (
         <div>
             <main className="flex min-h-screen flex-col items-center justify-between ">
