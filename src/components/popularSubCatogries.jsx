0f27ec05534d8bd5b8bce4ca65e6c49b95dd5e6c
Dipesh  Pawar <dipesh.p@renewhr.com>
2023-09-27 17:04:58 +0530

    fix sub Categories  section

--- src/components/popularSubCatogries.jsx
+++ src/components/popularSubCatogries.jsx
@@ -1,74 +1,48 @@
-import React from 'react';
-import Button from './atoms/button';
+import React from 'react'
+import Button from './atoms/button'
+import CategoriesCard from './atoms/categoriesCard.'
 
-async function PopularSubCategories({popularSubCategories :{ sub_categories : { data = [] } }}) {
+async function PopularSubCategories({
+    popularSubCategories: {
+        sub_categories: { data = [] },
+    },
+}) {
+    const popSubCat =
+        data?.map((entry) => {
+            return {
+                Id: entry.id,
+                ImgSrc: entry.attributes?.image?.data?.attributes?.url || '',
+                Name: entry.attributes?.name || '',
+            }
+        }) ?? []
 
-  const popSubCat = data?.map((entry) => {
-    return {
-      Id: entry.id,
-      ImgSrc: entry.attributes?.image?.data?.attributes?.url || '',
-      Name: entry.attributes?.name || '',
-    };
-  }) ?? [] ;
-
-  const reverseImg = [...popSubCat].reverse();
-  return (
-    <div className="w-full pb-5">
-      <h2 className="text-gray-900 font-manrope font-medium text-4xl lowercase">
-        popular sub-categories
-      </h2>
-      <div className="flex pt-10 gap-3">
-        {reverseImg.map((item) => (
-          <div
-            key={item.Id} // Use the Id as the key
-            className={`h-96 relative  `}
-          >
-            <img
-              src={item.ImgSrc}
-              className="w-full h-full rounded object-cover"
-              alt={`Category ${item.Id}`}
-            />
-            <Button
-              rounded="full"
-              variant="primary"
-              addStyle={
-                item.Id === 6
-                  ? "text-black border-2 border-black absolute top-7 left-7"
-                  : "text-white border-2 border-white absolute top-7 left-7"
-              }
-            >
-              <p style={{ fontSize: "16px" }}>{item.Name}</p>
-            </Button>
-          </div>
-        ))}
-      </div>
-      <div className="flex gap-3 pt-3">
-        {popSubCat.map((item) => (
-          <div
-            key={item.Id} // Use the Id as the key
-            className={`w-${item.Id <= 1 ? "4/12" : "6/12"} h-96 relative`}
-          >
-            <img
-              src={item.ImgSrc}
-              className="w-full h-full rounded object-cover"
-              alt={`Category ${item.Id}`}
-            />
-            <Button
-              rounded="full"
-              variant="primary"
-              addStyle={
-                item.Id === 6
-                  ? "text-black border-2 border-black absolute top-7 left-7"
-                  : "text-white border-2 border-white absolute top-7 left-7"
-              }
-            >
-              <p style={{ fontSize: "16px" }}>{item.Name}</p>
-            </Button>
-          </div>
-        ))}
-      </div>
-    </div>
-  );
+    return (
+        <div className="w-full pb-5">
+            <h2 className="text-gray-900 font-manrope font-medium text-4xl lowercase">
+                popular sub-categories
+            </h2>
+            <div className="flex pt-10 gap-3  flex-row flex-row-reverse">
+                {popSubCat.map((item, index) => (
+                    <div
+                        key={item.Id} // Use the Id as the key
+                        className={index == 2 ? 'basis-[46%]' : 'basis-[27%]'}
+                    >
+                        <CategoriesCard item={item} />
+                    </div>
+                ))}
+            </div>
+            <div className="flex mt-4 gap-3  flex-row ">
+                {popSubCat.map((item, index) => (
+                    <div
+                        key={item.Id} // Use the Id as the key
+                        className={index == 2 ? 'basis-[46%]' : 'basis-[27%]'}
+                    >
+                        <CategoriesCard item={item} />
+                    </div>
+                ))}
+            </div>
+        </div>
+    )
 }
 
-export default PopularSubCategories;
+export default PopularSubCategories
