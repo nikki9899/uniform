465cb8da1822a5b2e0bbb7f2dd85fceebe8691a4
nikki9899 <nikkidagar5316@gmail.com>
2023-10-11 21:45:06 +0530

    complete description

---
+++ src/utils/labels/descriptionLabels.js
@@ -0,0 +1,19 @@
+export const descriptionLabels={
+text:"Brand",
+text1:"Availability",
+text2:"MPN",
+text3:"Gross Weight",
+text4:"Delivery Time",
+text5:"Place of Manufacturing",
+data:"Pose",
+data1:"In Stock",
+data2:"222/17",
+data3:"200 to 300 GSM",
+data4:"20 to 30 days",
+data5:"New Delhi",
+heading:"Features",
+feature1:"1. lorem ipsum lorem ipsum lorem ipsum lorem ipsum",
+feature2:"2. lorem ipsum lorem ipsum lorem ipsum lorem ipsum",
+feature3:"3. lorem ipsum lorem ipsum lorem ipsum lorem ipsum"
+
+};
