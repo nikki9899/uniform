0ed4ced3c010bf6d691ae13c985f4ffe062141b9
nikki9899 <nikkidagar5316@gmail.com>
2023-10-23 20:15:18 +0530

    plp done

--- src/app/plp/[subCategory]/page.js
+++ src/app/plp/[subCategory]/page.js
@@ -1,36 +1,37 @@
-import Pagination from "@/components/atoms/Pagination"
-import IndustrialUniform from "@/components/industrialUniform"
-import Filter from "@/components/molecules/Filter"
-import { industrialCategoryMockData } from "@/mockData/category"
-import { getSubCategories } from "@/utils/api"
-import Image from "next/image"
+import Pagination from '@/components/atoms/Pagination'
+import IndustrialUniform from '@/components/industrialUniform'
+import Filter from '@/components/molecules/Filter'
+import { industrialCategoryMockData } from '@/mockData/category'
+import { getSubCategories } from '@/utils/api'
+import Image from 'next/image'
+import Subcategories from '@/components/organisms/subcategory'
+import { reflectiveClothingSubCategoryMockData } from '@/mockData/SubCategory'
+import Popular from '@/components/molecules/popular'
+import { getProducts } from '@/utils/api'
 
-const Plp =  async ( { params : {subCategory}  } ) => {
-    let  { data = [] , meta : { pagination : {  page  , pageSize ,pageCount , total}} } =  await getSubCategories(subCategory) ;
+const Plp = async ({ params: { subCategory } }) => {
+    let {
+        data = [],
+        meta: {
+            pagination: { page, pageSize, pageCount, total },
+        },
+    } = await getProducts(subCategory)
+    console.log(subCategory)
 
-let filterData = [] , finalData = []
+    let filterData = [],
+        finalData = []
 
     return (
         <div>
-        <main className="flex min-h-screen flex-col items-center justify-between ">
-            <IndustrialUniform heading={subCategory} />
-            <div className="h-px w-full mt-40 bg-black border-0 "></div>
-            <Filter filterData={filterData}/>
-            <div className="flex flex-col ">
-                {finalData.map(({ title, id, image }) => (
-                    <div>
-                        <h1>{title}</h1>
-                        <Image src={image} alt="img" width={150} height={300} />
-                    </div>
-                ))}
-            </div>
-            <Pagination totalPage={total} currPage={pageCount} />
-            <div className="h-px w-full my-8 bg-black border-0 "></div>
-          
-        </main>
-    </div>
-
-
+            <main className="flex min-h-screen flex-col items-center justify-between ">
+                <IndustrialUniform heading={subCategory} />
+                <div className="h-px w-full mt-40 bg-black border-0 "></div>
+                <Subcategories data={data} />
+                <Pagination totalPage={total} currPage={pageCount} />
+                <div className="h-px w-full my-8 bg-black border-0 "></div>
+                <Popular />
+            </main>
+        </div>
     )
 }
 
