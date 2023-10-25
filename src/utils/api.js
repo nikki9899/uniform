1e253c136ba78d71d8888b8317314758eceafd51
Dipesh  Pawar <dipesh.p@renewhr.com>
2023-10-25 13:41:27 +0530

    Merge branch 'Production' of https://github.com/definenext/uniform-on-web-fe into tabs

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,29 +1,47 @@
+const baseUrl = 'https://uniformonweb.onrender.com/api'
 
-const baseUrl = "https://uniformonweb.onrender.com/api";
-
-export const getAPI = async (path ) => {
-    let url = `${baseUrl}/${path}?populate=deep`;
+export const getAPI = async (path) => {
+    let url = `${baseUrl}/${path}?populate=deep`
 
     try {
-      const loadData = await fetch(url, { cache: 'force-cache' });
-  
-      if (loadData.ok) {
-        let result = await loadData.json();
-        return result;
-      } else {
-         return loadData;
-      }
-    } catch (error) {
-      throw error;
-    }
-};
+        const loadData = await fetch(url, { cache: 'force-cache' })
 
-export const getSubCategories = async ( category) =>{
-   let SubCategoryUrl  = `sub-categories?polulate=*&filters[category][name][$eq]=${category}`
-      return  await getAPI(SubCategoryUrl) ;
+        if (loadData.ok) {
+            let result = await loadData.json()
+            return result
+        } else {
+            return loadData
+        }
+    } catch (error) {
+        throw error
+    }
 }
 
+export const getSubCategories = async (category) => {
+    let SubCategoryUrl = `sub-categories?polulate=*&filters[category][name][$eq]=${category}`
+    return await getAPI(SubCategoryUrl)
+}
 
+export const getProducts = async (SubCategoryName) => {
+    let page = 1
+    let pageSize = 10
 
-//  get Product 
+    let ProductUrl = `https://uniformonweb.onrender.com/api/products?pagination[page]=${page}&pagination[pageSize]=${pageSize}&filters[sub_category][name][$eq]=${SubCategoryName}&populate=*`
+    try {
+        const loadData = await fetch(ProductUrl, { cache: 'force-cache' })
+
+        if (loadData.ok) {
+            let result = await loadData.json()
+            console.log(result)
+            return result
+        } else {
+            return loadData
+        }
+    } catch (error) {
+        throw error
+    }
+}
+
+//  get Product
 // https://uniformonweb.onrender.com/api/products?pagination[page]=1&pagination[pageSize]=10&filters[sub_category][name][$eq]=reflective%20clothing&populate=*
+
--- src/utils/labels/safetyLabels.js
+++ src/utils/labels/safetyLabels.js
@@ -1,4 +1,4 @@
 export const safetyLabels = {
   heading: " Full Body Safety Suit",
-  description: "Find safety suits pricing, cost & reviews",
+  description: "find safety suits pricing, cost & reviews",
 };
