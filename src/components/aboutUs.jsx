ee2d660c0048533c60ef6656ab40e5c561261e47
nikki9899 <nikkidagar5316@gmail.com>
2023-12-06 12:17:30 +0530

    aboutus

--- src/components/aboutUs.jsx
+++ src/components/aboutUs.jsx
@@ -1,19 +1,26 @@
-import Button from './atoms/button'
+import React from 'react';
+import Link from 'next/link';
+import Button from './atoms/button';
 
 const AboutUs = ({ aboutUs }) => {
-    return (
-        <div className="py-20 mx-auto max-w-screen-md text-center ">
-            <h3 className="text-7xl font-medium mb-2">About us</h3>
-            <h4 className=" text-3xl leading-relaxed py-6">
-                Your Uniform Experts Since 2000: Elevating Style and
-                Professionalism for Businesses and Schools.
-            </h4>
-            <div className="py-3">
-                <Button rounded="semi" variant="primary">
-                    <h5 className="text-2xl font-light">learn more</h5>
-                </Button>
-            </div>
-        </div>
-    )
-}
-export default AboutUs
+  const { title, description, button } = aboutUs;
+
+  return (
+    <div className="py-20 mx-auto max-w-screen-md text-center">
+      <h3 className="text-7xl font-medium mb-2">{title}</h3>
+      <h4 className="text-3xl leading-relaxed py-6">{description}</h4>
+      <div className="py-3">
+        <Link href="/aboutUs">
+        
+            <Button rounded="semi" variant="primary">
+              <h5 className="text-2xl font-light">{button.label}</h5>
+            </Button>
+         
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default AboutUs;
+
