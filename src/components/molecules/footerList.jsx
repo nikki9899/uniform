51c3895b7bb477d8b9358915505f1f6dbf0ea68a
nikki9899 <nikkidagar5316@gmail.com>
2024-01-24 23:17:55 +0530

    footerdone

--- src/components/molecules/footerList.jsx
+++ src/components/molecules/footerList.jsx
@@ -1,25 +1,25 @@
-import Link from "next/link";
-import { memo } from "react";
+import Link from 'next/link'
+import { memo } from 'react'
 
 const FooterList = (props) => {
-    let { title, listItems  } =  props.contents;
+    let { title, listItems } = props.contents
     return (
-      <div>
-        <h2 className="mb-4 text-sm font-semibold text-gray-900 ">
-          {title}
-        </h2>
-        <ul className="text-gray-700  font-normal">
-          {listItems.map(({ item, id , link = "" }) => (
-            <li key={id} className="mb-2">
-               <Link href={link}> {item}</Link>
-              {/* <a href="#" className=" hover:underline"> 
+        <div>
+            <h2 className="mb-4 md:text-sm  md:font-semibold text-gray-900 font-bold text-xs ">
+                {title}
+            </h2>
+            <ul className="text-gray-700  font-normal ">
+                {listItems.map(({ item, id, link = '' }) => (
+                    <li key={id} className="mb-2">
+                        <Link href={link}> {item}</Link>
+                        {/* <a href="#" className=" hover:underline"> 
                
               </a> */}
-            </li>
-          ))}
-        </ul>
-      </div>
-    );
-  };
+                    </li>
+                ))}
+            </ul>
+        </div>
+    )
+}
 
-  export default memo(FooterList) ;
+export default memo(FooterList)
