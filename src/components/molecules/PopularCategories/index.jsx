8796ee49f78ef02824b41932c00caaf60135fc0e
niteshn1 <niteshnegi50@gmail.com>
2024-02-15 20:57:49 +0530

    scrollbar

--- src/components/molecules/PopularCategories/index.jsx
+++ src/components/molecules/PopularCategories/index.jsx
@@ -2,6 +2,7 @@ import Carousel from '@/components/atoms/Crousel'
 import Image from 'next/image'
 import Button from '@/components/atoms/button'
 import Link from 'next/link'
+import '../../molecules/styles.css'
 
 const PopularCategories = async ({
     popularCategories: {
@@ -9,18 +10,11 @@ const PopularCategories = async ({
     },
 }) => {
     return (
-        <div className="w-full overflow:hidden">
-            <p className="text-left py-1 mb-2 md:mb-6 text-sm md:text-3xl font-medium leading-[26.645px]">
+        <div className="w-full pb-8">
+            <p className="text-left py-1 mb-2 md:mb-6 text-popularTextColor text-sm md:text-3xl not-italic font-normal leading-[26.645px]">
                 Popular Categories
             </p>
-
-            <Carousel
-                options={{
-                    dragFree: true,
-                    containScroll: 'trimSnaps',
-                }}
-                navigationButtons={true}
-            >
+            <div className="md:hidden mt-[20px] my-9 max-w-screen-full lg overflow-x-auto custom-scrollbar flex flex-nowrap">
                 {data.map(
                     (
                         {
@@ -41,15 +35,14 @@ const PopularCategories = async ({
                             <div
                                 className={`${
                                     (index + 1) % 3 === 0
-                                        ? 'md:basis-[46%] xs:basis-[50%] basis-[90%]' 
-                                        : 'md:basis-[27%] xs:basis-[50%] basis-[100%]'
-                                } grow-0 shrink-0 relative rounded-lg 
-                        }`}
+                                        ? 'md:basis-[46%] basis-[50%]'
+                                        : 'md:basis-[27%] basis-[50%]'
+                                } grow-0 shrink-0 relative rounded-lg`}
                                 key={id}
                             >
                                 <Link href={`/clp/${slug}`}>
                                     <Image
-                                        className="sm:h-[26rem] h-[205px] w-full rounded-lg object-cover object-top"
+                                        className="mb-[20px] h-[26rem] w-full rounded-lg object-cover object-top"
                                         width={500}
                                         height={500}
                                         src={url}
@@ -62,14 +55,76 @@ const PopularCategories = async ({
                                             ' text-white text-base border-1 border-white bg-slate-800 bg-opacity-30 absolute top-2.5 md:top-7 left-2 md:left-7'
                                         }
                                     >
-                                        <p className="text-[7.89px] md:text-base ">{name}</p>
+                                        <p className="text-[7.89px] md:text-base ">
+                                            {name}
+                                        </p>
                                     </Button>
                                 </Link>
                             </div>
                         )
                     }
                 )}
-            </Carousel>
+                {/* </div> */}
+            </div>
+            <div className="hidden md:block">
+                <Carousel
+                    options={{
+                        dragFree: true,
+                        containScroll: 'trimSnaps',
+                    }}
+                    navigationButtons={true}
+                >
+                    {data.map(
+                        (
+                            {
+                                id,
+                                attributes: {
+                                    name,
+                                    slug,
+                                    image: {
+                                        data: {
+                                            attributes: { url },
+                                        },
+                                    },
+                                },
+                            },
+                            index
+                        ) => {
+                            return (
+                                <div
+                                    className={`${
+                                        (index + 1) % 3 === 0
+                                            ? 'md:basis-[46%] basis-[50%]'
+                                            : 'md:basis-[27%] basis-[50%]'
+                                    } grow-0 shrink-0 relative rounded-lg`}
+                                    key={id}
+                                >
+                                    <Link href={`/clp/${slug}`}>
+                                        <Image
+                                            className="mb-[20px] h-[26rem] w-full rounded-lg object-cover object-top"
+                                            width={500}
+                                            height={500}
+                                            src={url}
+                                            alt={name}
+                                        />
+                                        <Button
+                                            rounded="full"
+                                            variant="primary"
+                                            addStyle={
+                                                ' text-white text-base border-1 border-white bg-slate-800 bg-opacity-30 absolute top-2.5 md:top-7 left-2 md:left-7'
+                                            }
+                                        >
+                                            <p className="text-[7.89px] md:text-base ">
+                                                {name}
+                                            </p>
+                                        </Button>
+                                    </Link>
+                                </div>
+                            )
+                        }
+                    )}
+                </Carousel>
+            </div>
         </div>
     )
 }
--- src/components/molecules/popular.jsx
+++ src/components/molecules/popular.jsx
@@ -3,41 +3,44 @@ import Button from '@/components/atoms/button'
 import ImageCard from '@/components/atoms/ImageCard'
 import { searchLabels } from '@/utils/labels/label'
 import Link from 'next/link'
-import "./style.css"
-const Popular =  ({ popularSearches : { products : { data = [] } = { data : [] } }  =   {  data : [] }  } ) => {
-     
-   
-    const items = data.map(({ id , attributes : {  Title  ,description , image } }) =>  (
-        <div className="me-7 w-3/4 md:w-full mb-12" key={id}>
-        <Link key={id} href={`/pdp/${id}`}>
-            <ImageCard
-                iconDisplay="flex"
-                classnames="w-[13rem] h-[18rem] md:w-64 md:h-[346px] lg:h-[418px] lg:w-[19rem] rounded-2xl shrink-0 back"
-                productImage={image.data.attributes.url}
-            />
-            </Link>
-            <div>
-            
-                <div className="flex justify-between my-1 ml-2">
-                    <h3 className="text-[9px] font-bold leading-4 sm:text-sm sm:leading-5 lg:text-sm lg:leading-6">{Title}</h3>
-                    <h4 className="text-[10px] sm:text-xs sm:leading-5 lg:text-sm lg:leading-5 font-bold leading-4">
+import './styles.css'
+const Popular = ({
+    popularSearches: { products: { data = [] } = { data: [] } } = { data: [] },
+}) => {
+    const items = data.map(
+        ({ id, attributes: { Title, description, image } }) => (
+            <div className="me-7 w-3/4 md:w-full mb-12" key={id}>
+                <Link key={id} href={`/pdp/${id}`}>
+                    <ImageCard
+                        iconDisplay="flex"
+                        classnames="w-[14rem] h-[18rem] md:w-[19rem] md:h-[26rem] rounded-2xl shrink-0 back"
+                        productImage={image.data.attributes.url}
+                    />
+                </Link>
+                <div>
+                    <div className="flex justify-between my-1 ml-2">
+                        <h3 className="text-sm font-semibold leading-7">
+                            {Title}
+                        </h3>
+                        {/* <h4 className="text-sm font-semibold leading-7">
                         INR 5000
-                    </h4>
-                </div>
-                <p className="text-xs text-[8px] leading-4 sm:text-[10px] sm:leading-5 lg:text-xs lg:leading-7 font-medium ml-2 text-start text-gray-500 ">
-                   hii
-                </p>
-                <div className="text-left w-[74px] h-6 sm:w-24 sm:h-8 lg:w-28 lg:h-10 ml-2 my-2">
-                    <Button variant="primary" rounded="full">
-                        <h2 className="text-[8px] sm:text-[10px] sm:leading-3 lg:text-xs lg:leading-4 font-medium leading-[10px]">{searchLabels.buttontext}</h2>
-                    </Button>
+                    </h4> */}
+                    </div>
+                    <p className="text-xs font-medium ml-2 text-start text-gray-500 leading-7">
+                        {description}
+                    </p>
+                    <div className="text-left ml-2 my-2">
+                        <Button variant="primary" rounded="full">
+                            {searchLabels.buttontext}
+                        </Button>
+                    </div>
                 </div>
             </div>
-        </div>
-    ))
+        )
+    )
     return (
         <div className="py-4 max-w-full  max-w-screen-full text-center ">
-            <div className="sm:text-3xl lg:text-4xl text-lg font-medium leading-7  mb-2 text-left">
+            <div className="text-3xl font-normal mb-2 text-left">
                 {searchLabels.heading}
             </div>
 
@@ -47,4 +50,4 @@ const Popular =  ({ popularSearches : { products : { data = [] } = { data : [] }
         </div>
     )
 }
-export default Popular
+export default Popular
